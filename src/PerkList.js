8bc9aab260d203704fa118f7625de1c569f2fbfb
MutlucanOzcan <mutlucanozcan@gmail.com>
2023-12-05 15:30:40 +0300

    perk api düzenlemesi ve bazı css düzenlemeleri

--- src/PerkList.js
+++ src/PerkList.js
@@ -1,48 +1,65 @@
 import React, { useState } from 'react';
-
+import './style.css';
 const DeadByDaylightPerks = () => {
-  const allPerks = [
-    {
-      name: "Self-Care",
-      effect: "Allows you to heal yourself without a medkit at 50% of the normal healing speed.",
-    },
-    {
-      name: "Sprint Burst",
-      effect: "When starting to run, break into a sprint at 150% of your normal running speed for a maximum of 3 seconds.",
-    },
-    {
-      name: "Borrowed Time",
-      effect: "When unhooking a Survivor, the unhooked Survivor and the Killer are protected by the Endurance status effect for 20 seconds.",
-    },
-    {
-      name: "Decisive Strike",
-      effect: "When the Killer downs you, you have a chance to strike back, stunning the Killer for a short duration if you succeed.",
-    },
-    {
-      name: "Adrenaline",
-      effect: "When the exit gates are powered, you and all other Survivors within a certain range are instantly healed, and you gain a speed boost for a short duration.",
-    },
-  ];
+  const [randomSurvivorPerks, setRandomSurvivorPerks] = useState([]);
+  const [randomKillerPerks, setRandomKillerPerks] = useState([]);
+  const [selectedRole, setSelectedRole] = useState(null);
 
-  const [randomPerks, setRandomPerks] = useState([]);
+  const generateRandomPerks = async (role) => {
+    try {
+      const response = await fetch(`https://dbd-proxy.vercel.app/api/randomPerks?role=${role}`);
+      const data = await response.json();
 
-  const generateRandomPerks = () => {
-    const shuffledPerks = [...allPerks].sort(() => Math.random() - 0.5);
-    const selectedPerks = shuffledPerks.slice(0, 4);
-    setRandomPerks(selectedPerks);
+      console.log(`${role} Perks:`, data);
+
+      if (Array.isArray(data.body)) {
+        if (role === 'survivor') {
+          setRandomSurvivorPerks(data.body);
+          setRandomKillerPerks([]);
+        } else if (role === 'killer') {
+          setRandomKillerPerks(data.body);
+          setRandomSurvivorPerks([]);
+        }
+        setSelectedRole(role);
+      } else {
+        console.error(`Invalid data format for ${role} perks:`, data);
+      }
+    } catch (error) {
+      console.error(`Error fetching ${role} perks:`, error);
+    }
+  };
+
+  const renderDescription = (description) => {
+    return { __html: description };
   };
 
   return (
     <div>
-      <h1>Dead by Daylight Perks</h1>
-      <button onClick={generateRandomPerks}>Generate Random Perks</button>
-      <ul>
-        {randomPerks.map((perk, index) => (
-          <li key={index}>
-            <strong>{perk.name}:</strong> {perk.effect}
+      <h1 className='center'>Dead by Daylight Perks</h1>
+      {selectedRole && (
+        <p className='center'>Selected Role - {selectedRole.charAt(0).toUpperCase() + selectedRole.slice(1)}</p>
+      )}
+      <button className='btn' onClick={() => generateRandomPerks('survivor')}>Generate Random Survivor Perks</button>
+      <ul className='list'>
+        {randomSurvivorPerks.map((perk, index) => (
+          <li className='list-item' key={index}>
+            <strong>{perk.name}:</strong>
+            <div dangerouslySetInnerHTML={renderDescription(perk.description)} />
           </li>
         ))}
       </ul>
+
+      <button className='btn' onClick={() => generateRandomPerks('killer')}>Generate Random Killer Perks</button>
+      <ul className='list'>
+        {randomKillerPerks.map((perk, index) => (
+          <li className='list-item' key={index}>
+            <strong>{perk.name}:</strong>
+            <div dangerouslySetInnerHTML={renderDescription(perk.description)} />
+          </li>
+        ))}
+      </ul>
+
+      
     </div>
   );
 };
